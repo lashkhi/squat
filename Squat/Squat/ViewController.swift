e3f36ff6b30c7b4917405fd6fd9a8916be31a944
David Lashkhi <lashkhi@me.com>
2016-10-17 23:06:50 +0300

    Project first setup

---
+++ Squat/Squat/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Squat
+//
+//  Created by David Lashkhi on 17/10/2016.
+//  Copyright © 2016 David Lashkhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func startCounting() {
+        
+    }
+
+}
+
